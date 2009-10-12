cb9a4f06f5594cfb45b689c27c0807bebf5bc20d
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2009-10-12 13:36:44 +0000

    cleanup
    git-svn-id: http://www.ontocat.org/svn/trunk@8 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoAPI/src/uk/ac/ebi/efo/api/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides the classes necessary to import annotations between ontologies.
- * 
- * @author $Id: package-info.java 8459 2009-08-17 11:41:55Z tomasz $
- */
-package uk.ac.ebi.efo.api;
