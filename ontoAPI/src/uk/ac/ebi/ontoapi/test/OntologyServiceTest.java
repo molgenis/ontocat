09cb3e1665f95d75af23aee81d50d022690c4b3e
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2010-01-01 11:43:29 +0000

    added FileOntologyServiceTest and required modfications to superclass junit test removed testGetLabelForAccession() from superclass junit test, as this is replicated already in EFOIDTranslatorTest
    git-svn-id: http://www.ontocat.org/svn/trunk@45 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoAPI/src/uk/ac/ebi/ontoapi/test/OntologyServiceTest.java
+++ ontoAPI/src/uk/ac/ebi/ontoapi/test/OntologyServiceTest.java
@@ -5,8 +5,6 @@ import java.util.Map.Entry;
 
 import org.junit.Test;
 
-import uk.ac.ebi.efo.bioportal.EFOIDTranslator;
-import uk.ac.ebi.efo.bioportal.IOntologyMapping;
 import uk.ac.ebi.ontoapi.Ontology;
 import uk.ac.ebi.ontoapi.OntologyService;
 import uk.ac.ebi.ontoapi.OntologyServiceException;
@@ -16,21 +14,8 @@ public abstract class OntologyServiceTest {
 	protected static OntologyService os;
 	protected static String ONTOLOGY_ACCESSION1;
 	protected static String ONTOLOGY_ACCESSION2;
-
-	@Test
-	public final void testGetLabelForAccession() {
-		printCurrentTest();
-		new EFOIDTranslator();
-		for (IOntologyMapping BPmap : new EFOIDTranslator().getMappings()) {
-			try {
-				String label = os.getTerm(BPmap.getOntologyAccession(), BPmap.getTestCode()).getLabel();
-				println(BPmap.getOntologyAccession() + " " + BPmap.getTestCode() + " " + label);
-			} catch (OntologyServiceException e) {
-				println(BPmap.getTestCode() + " NOT FOUND");
-			}
-		}
-		println();
-	}
+	protected static String TERM_ACCESSION1 = "GO:0009318";
+	protected static String TERM_ACCESSION2 = "GO:0005575";
 
 	@Test
 	public final void testGetOntologies() throws OntologyServiceException {
@@ -80,28 +65,32 @@ public abstract class OntologyServiceTest {
 	@Test
 	public final void testGetTermPath() throws OntologyServiceException {
 		printCurrentTest();
-		for (OntologyTerm ot : os.getTermPath(ONTOLOGY_ACCESSION2, "GO:0009318"))
+		for (OntologyTerm ot : os.getTermPath(ONTOLOGY_ACCESSION2,
+				TERM_ACCESSION1))
 			println(ot.getAccession() + " " + ot.getLabel());
 	}
 
 	@Test
 	public final void testGetChildren() throws OntologyServiceException {
 		printCurrentTest();
-		for (OntologyTerm ot : os.getChildren(ONTOLOGY_ACCESSION2, "GO:0005575"))
+		for (OntologyTerm ot : os.getChildren(ONTOLOGY_ACCESSION2,
+				TERM_ACCESSION1))
 			println(ot.getOntologyAccession() + " " + ot.getAccession() + " " + ot.getLabel());
 	}
 
 	@Test
 	public final void testGetParents() throws OntologyServiceException {
 		printCurrentTest();
-		for (OntologyTerm ot : os.getParents(ONTOLOGY_ACCESSION2, "GO:0009318"))
+		for (OntologyTerm ot : os.getParents(ONTOLOGY_ACCESSION2,
+				TERM_ACCESSION1))
 			println(ot.getOntologyAccession() + " " + ot.getAccession() + " " + ot.getLabel());
 	}
 
 	@Test
 	public final void testGetAnnotations() throws OntologyServiceException {
 		printCurrentTest();
-		for (Entry<String, String[]> e : os.getAnnotations(ONTOLOGY_ACCESSION2, "GO:0009318").entrySet()) {
+		for (Entry<String, String[]> e : os.getAnnotations(ONTOLOGY_ACCESSION2,
+				TERM_ACCESSION1).entrySet()) {
 			println(e.getKey());
 			for (String s : e.getValue()) {
 				println("\t" + s);
