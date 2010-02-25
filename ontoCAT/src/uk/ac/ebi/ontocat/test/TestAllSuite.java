e73839d81a8cbf2d3d04ecde26690aa96fa12744
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2010-02-25 11:49:31 +0000

    Adding test suite
    git-svn-id: http://www.ontocat.org/svn/trunk@107 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

---
+++ ontoCAT/src/uk/ac/ebi/ontocat/test/TestAllSuite.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.ontocat.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+// based on example taken from:
+// http://radio.javaranch.com/lasse/2006/07/27/1154024535662.html
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { BioportalServiceTest.class,
+		OlsOntologyServiceTest.class, FileOntologyServiceTest.class,
+		EFOIDTranslatorTest.class })
+public class TestAllSuite {
+	// the class remains completely empty,
+	// being used only as a holder for the above annotations
+}
