70e6d6f204c78a547af58abea35f9daf0e76c639
antonak <antonak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2010-03-09 13:41:14 +0000

    git-svn-id: http://www.ontocat.org/svn/trunk@125 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT-restservice/WebContent/SemOntoCat.js
+++ ontoCAT-restservice/WebContent/SemOntoCat.js
@@ -36,14 +36,7 @@ $(document).ready(function() {
                 selectedDropdownItem: "token-input-selected-dropdown-item-facebook",
                 inputToken: "token-input-input-token-facebook"
             
-           },
-	   onAddToken: function(args) {
-           	 // alter tokenInput itself to use "new/url/to/call" instead of 
-            	 // the original "url/to/call/"
-
-	       return "http://localhost:8080/ontocat/rest/json/searchAll/";
-           }
-        
+           }        
 	});
 
 	
@@ -75,10 +68,9 @@ $(document).ready(function() {
  });
 
 
-//	$.getJSON("http://localhost:9000/ontocat/json1/searchAll?q=brca2",function(json){
 $.getJSON("http://localhost:8080/ontocat/rest/json/searchAll/brca", 
         function(data){      
-	//the below work!
+			  //the below work!
         	  //alert("JSON Data 1: " + data.term[0].accession );
         	  //alert("JSON Data:2 " + data.term[0].label );
         	  //alert("JSON Data 3:" +  data.term[1].accession); 
@@ -87,9 +79,7 @@ $.getJSON("http://localhost:8080/ontocat/rest/json/searchAll/brca",
         });
 
 $.getJSON("http://localhost:8080/ontocat/rest/json/searchAll/brca",  function(json){
-		
 			// Parse JSON objects
-	 
 			 jJSON["accession"] = (function() {
 				response = {
 					values: [],
@@ -115,25 +105,14 @@ $.getJSON("http://localhost:8080/ontocat/rest/json/searchAll/brca",  function(js
 				return response;
 			})();
 
-			 /* Return a number of values for a given object */
-				//alert( jJSON.getValues("accession",null) );
-				//alert( jJSON.getValues("label",null) );
-				/* Return a count for a given object */
-				// alert( jJSON.getCount("label") );
-				/* Return a number of randomized values for a given object */
-				//alert( jJSON.getRandomValues("accession",null) ); 
-			
+			 
 			$("<div id='json_results2'><p>").appendTo("body");
 			$(jJSON.getValues("label",null)).appendTo("body");
 			$("<br/>").appendTo("body");
 			$(jJSON.getValues("accession",null)).appendTo("body");
 			$("<br/></div>").appendTo("body");
 
-			//send the above in autocomplete item
-			});
-
-
-
+	});
 
 
 var jJSON = {
@@ -164,31 +143,21 @@ var jJSON = {
 $.ajax({
   
    
-   beforeSend: function(){
-          // Handle the beforeSend event is called before the request is sent, and is passed the XMLHttpRequest object as a parameter.
+   beforeSend: function(){    // Handle the beforeSend event is called before the request is sent, and is passed the XMLHttpRequest object as a parameter.
    },
-   error: function() { 
-	   //is called if the request fails. It is passed the XMLHttpRequest object, a string indicating the error type, and an exception object if applicable.
+   error: function() {        //is called if the request fails. It is passed the XMLHttpRequest object, a string indicating the error type, and an exception object if applicable.
    },
-   
-   dataFilter: function() {
- 	  //is called on success. It is passed the returned data and the value of dataType, and must return the (possibly altered) data to pass on to success.
+   dataFilter: function() {	  //is called on success. It is passed the returned data and the value of dataType, and must return the (possibly altered) data to pass on to success.
    },
-
-   success: function (data) {
- 	//is called if the request succeeds. It is passed the returned data, a string containing the success code, and the XMLHttpRequest object.
-    	
+   success: function (data) { //is called if the request succeeds. It is passed the returned data, a string containing the success code, and the XMLHttpRequest object.	
    },
-
    complete: function(){  // Handle the complete event  complete is called when the request finishes, whether in failure or success. It is passed the XMLHttpRequest object, as well as a string containing the success or error code.
-		     var submitted_terms1 = $("#tokenize").val(); 
-			
-			//alert("Input:");
-			//alert(submitted_terms1);
-     
+	    var submitted_terms1 = $("#tokenize").val(); 
+	
+		//alert("Input:");
+		//alert(submitted_terms1); 
    }
-
-  // do we have to define var xmlhttp? in order to retrieve data from xmlhttp.responseText 
+  
  });
 
 
