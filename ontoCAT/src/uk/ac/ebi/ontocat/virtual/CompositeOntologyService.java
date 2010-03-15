47911d2b0e83ab284fbbce3dbd7c9ae7ac8faacf
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2010-03-15 17:55:38 +0000

    Extented CompositeOntologyService to use threads (only searchAll though)
    git-svn-id: http://www.ontocat.org/svn/trunk@133 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT/src/uk/ac/ebi/ontocat/virtual/CompositeOntologyService.java
+++ ontoCAT/src/uk/ac/ebi/ontocat/virtual/CompositeOntologyService.java
@@ -4,16 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import uk.ac.ebi.ontocat.AbstractOntologyService;
 import uk.ac.ebi.ontocat.Ontology;
 import uk.ac.ebi.ontocat.OntologyService;
 import uk.ac.ebi.ontocat.OntologyServiceException;
 import uk.ac.ebi.ontocat.OntologyTerm;
 
+// TODO: Needs multithreading
+/**
+ * The Class CompositeOntologyService. Combines multiple OntologyServices into
+ * one.
+ */
 public class CompositeOntologyService extends AbstractOntologyService {
 
+	/** The onto services. */
 	private List<OntologyService> ontoServices;
 
+	/** The Constant log. */
+	private static final Logger log = Logger
+			.getLogger(CompositeOntologyService.class);
+
+	/**
+	 * Instantiates a new composite ontology service.
+	 * 
+	 * @param ontoServices
+	 *            the onto services
+	 */
 	public CompositeOntologyService(List<OntologyService> ontoServices) {
 		this.ontoServices = ontoServices;
 	}
@@ -21,6 +39,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 	// /////////////////////////////////////
 	// pattern for Map<String, List<String>
 	// /////////////////////////////////////
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getAnnotations(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public Map<String, List<String>> getAnnotations(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -33,6 +57,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getRelations(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public Map<String, List<String>> getRelations(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -44,6 +74,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 	// pattern for List<String>
 	// /////////////////////////////////////
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getDefinitions(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<String> getDefinitions(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -56,6 +92,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getSynonyms(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<String> getSynonyms(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -71,6 +113,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 	// /////////////////////////////////////
 	// pattern for List<OntologyTerm>
 	// /////////////////////////////////////
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getChildren(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<OntologyTerm> getChildren(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -83,6 +131,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getParents(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<OntologyTerm> getParents(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -95,6 +149,11 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getRootTerms(java.lang.String)
+	 */
 	@Override
 	public List<OntologyTerm> getRootTerms(String ontologyAccession)
 			throws OntologyServiceException {
@@ -106,6 +165,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getTermPath(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<OntologyTerm> getTermPath(String ontologyAccession,
 			String termAccession) throws OntologyServiceException {
@@ -118,6 +183,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#searchOntology(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public List<OntologyTerm> searchOntology(String ontologyAccession,
 			String keywords) throws OntologyServiceException {
@@ -134,6 +205,11 @@ public class CompositeOntologyService extends AbstractOntologyService {
 	// pattern for combining results
 	// /////////////////////////////////////
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getOntologies()
+	 */
 	@Override
 	public List<Ontology> getOntologies() throws OntologyServiceException {
 		List<Ontology> result = new ArrayList<Ontology>();
@@ -143,14 +219,58 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return result;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#searchAll(java.lang.String)
+	 */
+	List<OntologyTerm> result = new ArrayList<OntologyTerm>();
+
 	@Override
 	public List<OntologyTerm> searchAll(String keywords)
 			throws OntologyServiceException {
-		List<OntologyTerm> result = new ArrayList<OntologyTerm>();
+
+		List<SearchThread> threads = new ArrayList<SearchThread>();
 		for (OntologyService os : ontoServices) {
-			result.addAll(os.searchAll(keywords));
+			SearchThread st = new SearchThread(keywords, os);
+			threads.add(st);
+			st.start();
 		}
+		// Wait for the threads to finish
+		for (SearchThread st : threads) {
+			try {
+				st.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return result;
+	}
+
+	private class SearchThread extends Thread {
+		private String keywords;
+		private OntologyService os;
+
+		/**
+		 * @param result
+		 * @param keywords
+		 * @param os
+		 */
+		public SearchThread(String keywords, OntologyService os) {
+			this.keywords = keywords;
+			this.os = os;
+		}
+
+		public void run() {
+			try {
+				synchronized (result) {
+					result.addAll(os.searchAll(keywords));
+				}
+			} catch (OntologyServiceException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	// /////////////////////////////////////
@@ -159,6 +279,11 @@ public class CompositeOntologyService extends AbstractOntologyService {
 
 	// TODO: This is not working properly, as OLS instantiates an
 	// empty ontology here for any parameter
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getOntology(java.lang.String)
+	 */
 	@Override
 	public Ontology getOntology(String ontologyAccession)
 			throws OntologyServiceException {
@@ -170,6 +295,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getTerm(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public OntologyTerm getTerm(String ontologyAccession, String termAccession)
 			throws OntologyServiceException {
@@ -181,6 +312,12 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * uk.ac.ebi.ontocat.OntologyService#makeLookupHyperlink(java.lang.String)
+	 */
 	@Override
 	public String makeLookupHyperlink(String termAccession) {
 		for (OntologyService os : ontoServices) {
@@ -191,6 +328,13 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * uk.ac.ebi.ontocat.OntologyService#makeLookupHyperlink(java.lang.String,
+	 * java.lang.String)
+	 */
 	@Override
 	public String makeLookupHyperlink(String ontologyAccession,
 			String termAccession) {
@@ -203,17 +347,20 @@ public class CompositeOntologyService extends AbstractOntologyService {
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see uk.ac.ebi.ontocat.OntologyService#getTerm(java.lang.String)
+	 */
 	@Override
 	public OntologyTerm getTerm(String termAccession)
 			throws OntologyServiceException {
-	for (OntologyService os : ontoServices) {
-		OntologyTerm result = os.getTerm( termAccession);
-		if (result != null)
-			return result;
+		for (OntologyService os : ontoServices) {
+			OntologyTerm result = os.getTerm(termAccession);
+			if (result != null)
+				return result;
+		}
+		return null;
 	}
-	return null;
-	}
-
-
 
 }
