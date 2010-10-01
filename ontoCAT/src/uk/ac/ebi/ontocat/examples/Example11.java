e9b1922af1e15ce661359074c0f567a98d20e1a2
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2010-10-01 15:35:45 +0000

    fixed the example for the Bleeding History Phenotype ontology
    git-svn-id: http://www.ontocat.org/svn/trunk@247 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT/src/uk/ac/ebi/ontocat/examples/Example11.java
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/Example11.java
@@ -1,11 +1,14 @@
 package uk.ac.ebi.ontocat.examples;
 
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
 
 import uk.ac.ebi.ontocat.OntologyService;
 import uk.ac.ebi.ontocat.OntologyServiceException;
 import uk.ac.ebi.ontocat.OntologyTerm;
 import uk.ac.ebi.ontocat.bioportal.BioportalOntologyService;
+import uk.ac.ebi.ontocat.virtual.CachedServiceDecorator;
 
 /**
  * Example 11
@@ -15,26 +18,41 @@ import uk.ac.ebi.ontocat.bioportal.BioportalOntologyService;
 public class Example11 {
 
 	public static void main(String[] args) throws OntologyServiceException, URISyntaxException {
-		// Instantiate BioPortal service
-		OntologyService os = new BioportalOntologyService();
+		// Instantiate BioPortal service with caching
+		OntologyService os = CachedServiceDecorator.getService(new BioportalOntologyService());
 
-		// Query NCI Thesaurus
-		OntologyTerm t1 = os.searchOntology("1032", "Malignant Neoplasm").get(0);
-		System.out.println(t1.getLabel());
-		for (String cui : os.getAnnotations(t1).get("UMLS_CUI"))
-			System.out.println(cui);
+		// Load all terms from an external ontology
+		System.out.println("Processing " + os.getOntology("1116").getLabel());
+		Set<OntologyTerm> terms = os.getAllTerms("1116");
+		System.out.println("Loaded " + terms.size() + " classes");
 
-		// Query AIR
-		OntologyTerm t2 = os.getTerm("1430", "U000045");
-		System.out.println(t2.getLabel());
-		for (String cui : os.getAnnotations(t2).get("UMLS_CUI"))
-			System.out.println(cui);
-
-		// Query National drug file
-		OntologyTerm t3 = os.getTerm("1352", "C7764591428519");
-		System.out.println(t3.getLabel());
-		for (String cui : os.getAnnotations("1352", "C7764591428519").get("UMLS_CUI"))
-			System.out.println(cui);
-
+		// And find their UMLS CUIs via NCI Thesaurus
+		for (OntologyTerm term : terms) {
+			String label = term.getLabel();
+			System.out.println("Searching for " + term);
+			List<OntologyTerm> searchResult = os.searchOntology("1032", label);
+			// multiple results
+			if (searchResult.size() > 1)
+				System.out.println("\tMultiple matches found");
+			// no results
+			if (searchResult.size() == 0) {
+				System.out.println("\tNo match found");
+				// ok lets choose the first term from the result list
+				// NOTE: this is not necessarily the most appropriate match
+				// more advanced ontology matching tools are available
+				// from http://www.ebi.ac.uk/efo/tools
+			} else {
+				OntologyTerm tNCI = searchResult.get(0);
+				List<String> cuis = os.getAnnotations(tNCI).get("UMLS_CUI");
+				if (cuis != null) {
+					// there can be multiple CUIs on a term
+					for (String cui : cuis)
+						System.out.println("\t" + cui + " " + tNCI.getLabel());
+					// or none at all
+				} else {
+					System.out.println("\tNo CUI on " + tNCI.getLabel());
+				}
+			}
+		}
 	}
 }
