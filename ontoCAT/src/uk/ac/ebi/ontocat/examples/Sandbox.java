a6c5c767cd8ec0e47e57421bb4082b738fae1209
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2011-02-07 10:57:29 +0000

    placeholder for some testing
    git-svn-id: http://www.ontocat.org/svn/trunk@307 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

---
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/Sandbox.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.ontocat.examples;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import uk.ac.ebi.ontocat.OntologyService;
+import uk.ac.ebi.ontocat.OntologyService.SearchOptions;
+import uk.ac.ebi.ontocat.OntologyServiceException;
+import uk.ac.ebi.ontocat.OntologyTerm;
+import uk.ac.ebi.ontocat.file.FileOntologyService;
+
+public class Sandbox {
+
+	/**
+	 * @param args
+	 * @throws URISyntaxException
+	 * @throws OntologyServiceException
+	 */
+	public static void main(String[] args) throws URISyntaxException,
+			OntologyServiceException {
+		OntologyService os = new FileOntologyService(
+				new URI(
+						"http://efo.svn.sourceforge.net/svnroot/efo/trunk/src/efoinowl/efo.owl"));
+
+		for (OntologyTerm ot : os.searchAll("human",
+				SearchOptions.INCLUDE_PROPERTIES, SearchOptions.EXACT)) {
+			System.out.println(ot);
+		}
+	}
+}
