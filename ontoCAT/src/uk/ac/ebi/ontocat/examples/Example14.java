547b867f8a7294d0644b9105576932f41975c77f
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2011-03-09 15:27:15 +0000

    clenaup
    git-svn-id: http://www.ontocat.org/svn/trunk@339 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT/src/uk/ac/ebi/ontocat/examples/Example14.java
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/Example14.java
@@ -1,19 +1,17 @@
 package uk.ac.ebi.ontocat.examples;
 
 import java.net.URI;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import uk.ac.ebi.ontocat.OntologyServiceException;
 import uk.ac.ebi.ontocat.OntologyTerm;
 import uk.ac.ebi.ontocat.file.ReasonedFileOntologyService;
+
 
 /**
  * Example 13
@@ -22,7 +20,6 @@ import uk.ac.ebi.ontocat.file.ReasonedFileOntologyService;
  */
 public class Example14 {
 	private static final Logger log = Logger.getLogger(Example14.class);
-	private static HashMap<OntologyTerm, Set<OntologyTerm>> parts;
 	private static ReasonedFileOntologyService os;
 
 	/**
@@ -38,25 +35,25 @@ public class Example14 {
 		// Note behind the scenes that uses the HermiT reasoner
 		// http://hermit-reasoner.com/
 		os = new ReasonedFileOntologyService(new URI(
-				"http://www.ebi.ac.uk/efo/efo.owl"), "efo");
+		"http://www.ebi.ac.uk/efo/efo.owl"), "efo");
 
 		// Get parents of atrial myocardium (EFO_0003087)
 		// This is a defined class and it would not work
 		// otherwise
 		log.info(os.getParents("efo", "EFO_0003087"));
 
-		// Build partonomy - to make it quicker we'll focus 
+		// Build partonomy - to make it quicker we'll focus
 		// on the skeleton structure branch (EFO_0000806)
 		// to compute whole partonomy try organism part (EFO_0000635)
-		OntologyTerm rootNode = os.getTerm("EFO_0000806");
+		OntologyTerm startNode = os.getTerm("EFO_0000806");
 
 		// But first lets see all the possible relations for this term
-		Map<String, Set<OntologyTerm>> result = os.getRelations(rootNode);
+		Map<String, Set<OntologyTerm>> result = os.getRelations(startNode);
 
 		for (Entry<String, Set<OntologyTerm>> entry : result.entrySet()) {
 			System.out.println("\n\t" + entry.getKey());
 			for (OntologyTerm ot : entry.getValue()) {
-				System.out.println(ot);
+				System.out.println(ot.getLabel());
 			}
 		}
 
@@ -64,13 +61,13 @@ public class Example14 {
 		// Note that other axes are also possible
 		// and we're using a specific getRelationsShortcut()
 		// to only compute the has_part relations
-		Set<OntologyTerm> branch = os.getAllChildren(rootNode);
-		branch.add(rootNode);
+		Set<OntologyTerm> branch = os.getAllChildren(startNode);
+		branch.add(startNode);
 		log.info("Processing partonomy for " + branch.size() + " classes");
-		
+
 		// Visualise recursively
-		System.out.println(rootNode.getLabel());
-		visualise(rootNode, "    ");
+		System.out.println(startNode.getLabel());
+		visualise(startNode, "    ");
 
 	}
 
@@ -85,18 +82,17 @@ public class Example14 {
 	 *             the ontology service exception
 	 */
 	private static void visualise(OntologyTerm currentNode, String tab)
-			throws OntologyServiceException {
+	throws OntologyServiceException {
 		String partPadding = "|---has_part--- ";
 		String isaPadding = "|-------------- ";
 		String newTab = tab
-				+ String.format("%1$-" + partPadding.length() + "s", "");
+		+ String.format("%1$-" + partPadding.length() + "s", "");
 
 		Set<OntologyTerm> isa_children = new HashSet<OntologyTerm>(
 				os.getChildren(currentNode));
 		Set<OntologyTerm> part_children = new HashSet<OntologyTerm>(os
 				.getRelationsShortcut(currentNode.getOntologyAccession(),
 						currentNode.getAccession(), "has_part").get("has_part"));
-
 
 		// remove has_part children from the asserted isa set
 		isa_children.removeAll(part_children);
