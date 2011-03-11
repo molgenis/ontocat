bf3a5389ca1d8de42ba9ad7fc30ee80a9d87b64c
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2011-03-11 19:50:34 +0000

    git-svn-id: http://www.ontocat.org/svn/trunk@379 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT/test/uk/ac/ebi/ontocat/HudsonTest.java
+++ ontoCAT/test/uk/ac/ebi/ontocat/HudsonTest.java
@@ -1,9 +1,13 @@
 package uk.ac.ebi.ontocat;
 
+import junit.framework.Assert;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import uk.ac.ebi.ontocat.bioportal.BioportalOntologyService;
 import uk.ac.ebi.ontocat.special.AbstractOntologyServiceTest;
@@ -31,6 +35,19 @@ public class HudsonTest extends AbstractOntologyServiceTest {
 	public void runAfterEveryTest() {
 		t2 = System.nanoTime();
 		log.info("Query took " + ((t2 - t1) * 1e-6) + " ms\t");
+	}
+
+	@Test
+	public void instantiateTest() {
+		System.out.println("Instantiating cache");
+		System.setProperty("net.sf.ehcache.enableShutdownHook", "true");
+		CacheManager manager = CacheManager.create(getClass().getResource(
+		"ehcache.xml"));
+		Assert.assertNotNull("manager null", manager);
+		Assert.assertNotNull("resource null",
+				getClass().getResource("ehcache.xml"));
+		Cache ServiceCache = manager.getCache("OntologyServiceCache");
+		Assert.assertNotNull("service cache null", ServiceCache);
 	}
 
 }
