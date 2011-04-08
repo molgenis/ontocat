3abd1b43f33d28b3cdcadfb0ea342146dee0b243
nata_courby <nata_courby@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2011-04-08 22:59:30 +0000

    R examples
    git-svn-id: http://www.ontocat.org/svn/trunk@436 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

---
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/R/Example1.R
@@ -0,0 +1,59 @@
+#Gene enrichment test by using ontoCAT R package
+library(RCurl)
+library(rjson)
+
+# Define a function to retrieve some genes from Gene Expression Atlas (http://www.ebi.ac.uk/gxa/)
+options(GXAAPIURL="http://www.ebi.ac.uk/gxa/api?")
+queryGeneUp <- function(query) fromJSON(getURL(paste(getOption("GXAAPIURL"), "upIn=", query, sep = "")))
+
+# Create a list of over-expressed genes in pancreas vs other tissues
+g <- queryGeneUp("pancreas")
+
+# g$results is a list of 100 genes, each accessible by g$results[[i]]$gene
+names(g$results[[10]]$gene)
+
+# Get GO ontology
+library(ontoCAT)
+
+# Obtain GO slim for biological processes:
+go <- getOntology("http://www.ontocat.org/browser/trunk/ontoCAT/src/uk/ac/ebi/ontocat/examples/R/resources/goslim_bp.obo")
+
+# Obtain the list of all GOIDs using biomaRt
+library(biomaRt)
+mart <- useMart("ensembl")
+mart<-useDataset("hsapiens_gene_ensembl",mart)
+genes <- getBM(attributes=c("ensembl_gene_id","go_biological_process_id"), mart=mart)
+all.GOIDs <- unique(genes[,2])
+all.GOIDs <- all.GOIDs[2:length(all.GOIDs)]
+
+# Create a list of over-expressed genes
+genes.of.interest <- c(sapply(1:100,function(x) g$results[[x]]$gene$ensemblGeneId),recursive = TRUE)
+selection <- genes[which(genes[,1] %in% genes.of.interest),]
+
+# Create a list of genes for each GO ID
+genes.by.GOID <- sapply(1:length(all.GOIDs),function(x) genes[which(genes[,2] == all.GOIDs[x]),][1]$ensembl_gene_id)
+names(genes.by.GOID)=all.GOIDs
+
+# Create a list containing the number of genes in each category
+size.by.GOID<-lapply(genes.by.GOID,length)
+
+# Construct a list containing the overlap for each GO category with the genes of interest
+#by using ontoCAT library to get all children (subclasses) of particular GO term
+overlaps = lapply(names(genes.by.GOID), function(x) length(which(selection[,2] %in%
+gsub("GO_*", "GO:", c(lapply(getTermAndAllChildrenById(go,toString(gsub("GO:*", "GO_",x))),function(t) {getAccession(t)}),recursive = TRUE))
+)))
+
+# Create a list of hypergeometric p-values
+pvals = as.array(sapply(names(genes.by.GOID), function(x)
+    phyper(overlaps[[x]]-1,
+    size.by.GOID[[x]],
+    length(all.GOIDs) - size.by.GOID[[x]],
+    length(unique(selection[,1])),
+    lower.tail=FALSE)))
+
+# Get the most enriched categories:
+pvals[order(pvals)][1:10]
+
+
+
+
