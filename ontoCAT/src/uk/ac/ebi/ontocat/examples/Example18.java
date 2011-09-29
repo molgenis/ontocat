0f2b3838d9c5d7e7fe46db2f44febba744dc6f22
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2011-09-29 23:58:35 +0000

    namespaces example
    git-svn-id: http://www.ontocat.org/svn/trunk@500 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

---
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/Example18.java
@@ -0,0 +1,76 @@
+// Obtain hierarchy information for a term in an ontology
+/**
+ * Copyright (c) 2010 - 2011 European Molecular Biology Laboratory and University of Groningen
+ *
+ * Contact: ontocat-users@lists.sourceforge.net
+ * 
+ * This file is part of OntoCAT
+ * 
+ * OntoCAT is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * OntoCAT is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with OntoCAT. If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.ontocat.examples;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.ebi.ontocat.OntologyService.SearchOptions;
+import uk.ac.ebi.ontocat.OntologyServiceException;
+import uk.ac.ebi.ontocat.OntologyTerm;
+import uk.ac.ebi.ontocat.file.FileOntologyService;
+
+/**
+ * Example 18
+ * 
+ * The tricky namespaces
+ */
+public class Example18 {
+	private static final Logger log = Logger.getLogger(Example12.class);
+
+	public static void main(String[] args) throws OntologyServiceException, URISyntaxException {
+		// OWL API arbitrary assigns one to OBO ontologies as they are not supported 
+		// natively by OBO format 
+		// OWL ontologies can have multiple namespaces if they import terms directly (e.g. EFO). 
+		// In practice anything that comes up in the file minus URI fragment can be used as 
+		// ontology accession, but it is good practice to specify one yoursefl in alternative
+		// constructor, see below:
+
+		// Instantiate a FileOntologyService(URI, ontologyAccession)
+		FileOntologyService os = new FileOntologyService(new URI("http://www.co-ode.org/ontologies/pizza/2007/02/12/pizza.owl"), 
+				"http://pizza/namespace/");
+	
+		// Try to find a term
+		List<OntologyTerm> list = os.searchAll("Cajun");
+
+		System.out.println(list.get(0).getOntologyAccession());
+		// http://www.co-ode.org/ontologies/pizza/pizza.owl#
+
+	    System.out.println(os.getOntologies().get(0).getOntologyAccession());
+	    // http://www.co-ode.org/ontologies/pizza/pizza.owl
+
+	    // Those work
+	    System.out.println(os.getAllTerms("http://www.co-ode.org/ontologies/pizza/pizza.owl#").size());
+	    System.out.println(os.getAllTerms("http://pizza/namespace/").size());
+	    System.out.println(os.getRootTerms("http://www.co-ode.org/ontologies/pizza/pizza.owl#").size());
+	    System.out.println(os.getRootTerms("http://pizza/namespace/").size());
+	    
+	    // Those don't
+	    System.out.println(os.getAllTerms("http://www.co-ode.org/ontologies/pizza/pizza.owl").size());
+	    System.out.println(os.getAllTerms(null).size());
+	    System.out.println(os.getRootTerms("http://www.co-ode.org/ontologies/pizza/pizza.owl").size());
+	    System.out.println(os.getRootTerms((String)null).size());
+	}
+}
