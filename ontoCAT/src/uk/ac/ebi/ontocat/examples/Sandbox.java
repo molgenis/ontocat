ca54ac7a560523172a62a581c29984f5999c2d00
tadamusiak <tadamusiak@f0dfcaea-3b95-4152-9e97-e99b8c58adb7>
2012-12-07 17:19:16 +0000

    git-svn-id: http://www.ontocat.org/svn/trunk@550 f0dfcaea-3b95-4152-9e97-e99b8c58adb7

--- ontoCAT/src/uk/ac/ebi/ontocat/examples/Sandbox.java
+++ ontoCAT/src/uk/ac/ebi/ontocat/examples/Sandbox.java
@@ -2,32 +2,33 @@
  * Copyright (c) 2010 - 2011 European Molecular Biology Laboratory and University of Groningen
  *
  * Contact: ontocat-users@lists.sourceforge.net
- * 
+ *
  * This file is part of OntoCAT
- * 
+ *
  * OntoCAT is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * OntoCAT is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along
  * with OntoCAT. If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.ontocat.examples;
 
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import uk.ac.ebi.ontocat.OntologyService;
 import uk.ac.ebi.ontocat.OntologyServiceException;
-import uk.ac.ebi.ontocat.ols.OlsOntologyService;
+import uk.ac.ebi.ontocat.OntologyTerm;
+import uk.ac.ebi.ontocat.file.FileOntologyService;
 
 public class Sandbox {
 
@@ -39,10 +40,18 @@ public class Sandbox {
 	 */
 	public static void main(String[] args) throws OntologyServiceException {
 
-		OntologyService os = new OlsOntologyService();
-		Map<String, List<String>> annotations = os.getAnnotations("DOID",
-				"DOID:0050120");
-		System.out.println(annotations.get("xref_analog"));
+		OntologyService os = new FileOntologyService(
+				new File(
+						"C:\\Users\\tadamusiak\\Dropbox\\docs\\papers\\2012-AMIA_loinc\\loinc_condor_inferred.owl")
+						.toURI());
+		// "C:\\Users\\tadamusiak\\Dropbox\\docs\\papers\\2012-AMIA_loinc\\data\\loinc_inferred.owl")
+
+
+		// gluose | urine
+		OntologyTerm ot = os.getTerm("OBS_LP44799-2");
+		Set<OntologyTerm> set = os.getAllChildren(ot.getOntologyAccession(),
+				ot.getAccession());
+		System.out.println(set.size());
 
 	}
 }
